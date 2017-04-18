71aa7ac6e65a00e9a9cc37a280de98531a3e8ba7
gymreklab <melissa.gymrek@gmail.com>
2017-04-18 15:12:04 -0700

    adding vis scripts

--- gatk-hc/run_gatk.sh
+++ gatk-hc/run_gatk.sh
@@ -2,6 +2,7 @@
 
 GATK=/storage/resources/source/GenomeAnalysisTK.jar
 REFFA=/storage/resources/dbase/human/hs37d5/hs37d5.fa
+BUNDLE=/storage/resources/dbase/human/hs37d5/gatk_resource_bundle/
 
 # For testing
 TESTGVCF1=/storage/s1saini/VCF/gvcf/11002/Sample_SSC03070/analysis/SSC03070.haplotypeCalls.raw.gvcf.vcf.gz
@@ -13,11 +14,24 @@ OUTDIR=/storage/mgymrek/ssc-imputation/gatk-hc
 # CombineGVCFs - recommend batches of ~200 gVCFs into one
 
 # Joint genotyping - GenotypeGVCFs
-java -jar ${GATK} \
-    -T GenotypeGVCFs \
-    -R ${REFFA} \
-    --variant ${TESTGVCF1} \
-    --variant ${TESTGVCF2} \
-    -o ${OUTDIR}/test.vcf
+#java -jar ${GATK} \
+#    -T GenotypeGVCFs \
+#    -R ${REFFA} \
+#    --variant ${TESTGVCF1} \
+#    --variant ${TESTGVCF2} \
+#    -o ${OUTDIR}/test.vcf
 
 # Classic GATK Best Practices
+java -jar ${GATK} \
+    -T VariantRecalibrator \
+    -R ${REFFA} \
+    -input ${OUTDIR}/test.vcf \
+    -resource:hapmap,known=false,training=true,truth=true,prior=15.0 ${BUNDLE}/hapmap_3.3.b37.vcf \
+    -resource:omni,known=false,training=true,truth=false,prior=12.0 ${BUNDLE}/1000G_omni2.5.b37.vcf \
+    -resource:1000G,known=false,training=true,truth=false,prior=10.0 ${BUNDLE}/1000G_phase1.snps.high_confidence.b37.vcf \
+    -resource:dbsnp,known=true,training=false,truth=false,prior=2.0 ${BUNDLE}/dbsnp_138.b37.vcf \
+    -mode SNP \
+    -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR -an DP \
+    -recalFile ${OUTDIR}/test.recal \
+    -tranchesFile ${OUTDIR}/test.tranche \
+    -rscriptFile ${OUTDIR}/test.plots.R
