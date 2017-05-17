120ad8da1db67b5655aabaac9833e1787b591f4c
gymreklab <melissa.gymrek@gmail.com>
2017-05-17 15:34:04 -0700

    removing debugging info from aws startup

--- psmc/get_consensus.sh
+++ psmc/get_consensus.sh
@@ -7,7 +7,7 @@ OUTBUCKET=$2
 OUTFILE=$3
 REFFA=$4
 
-sudo samtools mpileup -C50 -uf $REFFA $BAMFILE | head -n 10000 | bcftools call -c - \
+sudo samtools mpileup -C50 -uf $REFFA $BAMFILE | bcftools call -c - \
       | vcfutils.pl vcf2fq -d 10 -D 100 | gzip > ${OUTFILE}
 aws s3 cp ${OUTFILE} ${OUTBUCKET}/$(basename ${OUTFILE})
 rm ${OUTFILE}
