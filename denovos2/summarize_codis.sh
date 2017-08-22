0c88c4e3d0d664f31be83027bd94d20a727a2f05
gymreklab <melissa.gymrek@gmail.com>
2017-08-22 10:36:12 -0700

    adding codis/disease loci

---
+++ denovos2/summarize_codis.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source params.sh
+
+cat ${OUTDIR}/*_bylength.locus_summary.tab | grep -v chrom | intersectBed -a stdin -b CODIS.bed -wa -wb | \
+    awk '{print $1 "\t" $2 "\t" $3 "\t" $4 "\t"$9 "\t" $10 "\t" $11 "\t" $23 "\t" $24 "\t" $19}'
