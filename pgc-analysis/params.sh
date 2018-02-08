fc18f73c482413f5a849a960c0ba223b990f2cd1
Melissa Gymrek <gymrek@test3.irc.sara.nl.irc.sara.nl>
2018-02-09 00:40:21 +0100

    adding params file

---
+++ pgc-analysis/params.sh
@@ -0,0 +1,8 @@
+OUTDIR=/home/gymrek/genomewide-imputation-pgc/
+SCRATCHDIR=${OUTDIR}/tmp/
+COVARFILE=/home/pgcdac/DWFV2CJb8Piv_0116_pgc_data/scz/wave2/v1/prune.bfile.cobg.PGC_SCZ49.sh2.menv.mds_cov
+COVARCOLS=C1,C2,C3,C4,C5,C6,C7,C9,C15,C18 # based on recommendation from S. Ripke
+
+COHORTFILES=/home/gymrek/pgc_imputation/pgc_eur_mergelist.txt
+
+BEAGLE=/home/gymrek/bin/beagle.08Jun17.d8b.jar
