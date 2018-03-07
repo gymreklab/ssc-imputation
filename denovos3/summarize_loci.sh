e5649a2fb59838765bf99894eb65683685d2e1ff
ileenamitra <ileenamitra@gmail.com>
2018-03-06 21:50:55 -0800

    update

--- denovos3/summarize_loci.sh
+++ denovos3/summarize_loci.sh
@@ -6,42 +6,29 @@
 #SBATCH -t 1000
 #SBATCH --mem=16gb
 #SBATCH -o /oasis/projects/nsf/csd568/ileena/ssc_phase1_denovos/filtered_mutations/logs/Summarize_SSC_Denovo_Loci.log
-#SBATCH -e /oasis/projects/nsf/csd568/ileena/ssc_phase1_denovos/filtered_mutations/logs/Summarize_SSC_Denovo_Loci.log                                                          
-#SBATCH --mail-user=ilmitra@ucsd.edu
+#SBATCH -e /oasis/projects/nsf/csd568/ileena/ssc_phase1_denovos/filtered_mutations/logs/Summarize_SSC_Denovo_Loci.log         #SBATCH --mail-user=ilmitra@ucsd.edu
 #SBATCH --mail-type=ALL
+#SBATCH --array=1-21
 
 date;hostname;pwd
 
 source /home/ileena/ssc-imputation/denovos3/params.sh
 
 #################### Update summary files ###########################
-for chrom in $(seq 1 22)
-do
-    cmd="/home/ileena/ssc-imputation/denovos3/summarize_loci.py \
+chrom=${SLURM_ARRAY_TASK_ID}
+/home/ileena/ssc-imputation/denovos3/summarize_loci.py \
 	--allmutations ${FINALOUTDIR}/denovos_bylength.all_mutations_filtered.tab.gz \
 	--loci ${FINALOUTDIR}/denovos_by_length_loci.bed \
+	--annotations ${ANNDIR}/denovo_annotations.bed \
 	--out ${OUTDIR}/denovos_chr${chrom}_bylength.locus_summary.tab \
 	--output-mutations ${OUTDIR}/denovos_chr${chrom}_bylength.all_mutations.tab \
 	--filter-both-kids \
 	--max-filtered-families ${MAXFILTFAM} \
 	--min-children ${MINCHILDREN} \
 	--chrom ${chrom} \
-	--pthresh ${PTHRESH} > ${LOGDIR}/chr${chrom}.summ.out 2>&1"
-    echo $cmd
-done | xargs -P22 -I% -n1 sh -c "%"
+	--pthresh ${PTHRESH} > ${LOGDIR}/chr${chrom}.summ.out 2>&1
 
-cat ${OUTDIR}/denovos_chr1_bylength.locus_summary.tab | head -n 1 > ${FINALOUTDIR}/denovos_bylength.locus_summary.bed
-cat ${OUTDIR}/denovos_chr1_bylength.all_mutations.tab | head -n 1 > ${FINALOUTDIR}/denovos_bylength.all_mutations_pass.tab
-for chrom in $(seq 1 22)
-do
-    cat ${OUTDIR}/denovos_chr${chrom}_bylength.locus_summary.tab | grep -v "^#" >> \
-	${FINALOUTDIR}/denovos_bylength.locus_summary.bed
-    cat ${OUTDIR}/denovos_chr${chrom}_bylength.all_mutations.tab | grep -v "^#" >> \
-	${FINALOUTDIR}/denovos_bylength.all_mutations_pass.tab
-done
 
-bgzip -f ${FINALOUTDIR}/denovos_bylength.locus_summary.bed
-tabix -p bed -f ${FINALOUTDIR}/denovos_bylength.locus_summary.bed.gz
-bgzip -f ${FINALOUTDIR}/denovos_bylength.all_mutations_pass.tab
-tabix -b 2 -e -2 -f ${FINALOUTDIR}/denovos_bylength.all_mutations_pass.tab.gz
+
+
 
